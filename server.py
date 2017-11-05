091ec68bb5d3dcd2e7616b14c546a6471872f2c9
Ayush Kohli <ayushk96@hotmail.com>
2017-11-04 20:54:57 -0500

    Server side genius call

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 import os, json
-from flask import Flask
+from flask import Flask, render_template, jsonify,request
 from watson_developer_cloud import AuthorizationV1 as Authorization
 from watson_developer_cloud import SpeechToTextV1 as SpeechToText
 from cred import speech_auth
+from py_genius import Genius
 
 # Speech to Text
 STT_USERNAME = speech_auth['username'] 
 STT_PASSWORD = speech_auth['password'] 
+GENIUS_TOKEN = speech_auth['auth']
 
 app = Flask(__name__, static_url_path='')
+api = Genius(GENIUS_TOKEN)
 
 @app.route('/')
 def root():
@@ -16,8 +19,19 @@ def root():
 
 @app.route('/api/speech-to-text/token')
 def getSttToken():
-	print(STT_USERNAME)
-	authorization = Authorization(username=STT_USERNAME, password=STT_PASSWORD)
-	return authorization.get_token(url=SpeechToText.default_url)
+  #print(STT_USERNAME)
+  authorization = Authorization(username=STT_USERNAME, password=STT_PASSWORD)
+  return authorization.get_token(url=SpeechToText.default_url)
+
+@app.route('/api/search',methods=['POST'])
+def get_quote():
+  #print request.json['search']
+  search=request.json['search']
+  result = api.search(search)
+  for sub_result in result['response']['hits'][:3]:
+    to_parse=sub_result['result']
+    print to_parse['title'],to_parse['primary_artist']['name']
+  return jsonify(result)
+
 
 app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 var stuff;
 document.querySelector('#button').onclick = function() {
-
     fetch('/api/speech-to-text/token')
         .then(function(response) {
             return response.text();
@@ -28,3 +27,31 @@ document.querySelector('#button').onclick = function() {
             console.log(error);
         });
 };
+
+function getSong() {
+    var xhr = new XMLHttpRequest(); //XML HTTP Request
+
+    //xhr.open("GET", API + stuff, false);
+    //xhr.setRequestHeader('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36')
+    //xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //xhr.setRequestHeader('Accept', 'application/json');
+    //xhr.setRequestHeader('Host', 'api.genius.com')
+    //xhr.setRequestHeader('Authorization', 'Bearer 7e-MLnwd98C0hxessa9wg5vxNWd0ib8T6T7wHiI__WyDNESmqp89_lxGZdBmToLg');
+    //console.log(stuff);
+    //xhr.send();
+
+    //xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                // Success! Do stuff with data.
+                //console.log(xhr.responseText);
+                var song_payload = xhr.response;
+                var json = JSON.parse(song_payload);
+                console.log(json);
+            }
+        }
+    };
+
+}
